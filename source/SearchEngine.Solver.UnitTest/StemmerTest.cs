e3c2960935e0a53d1a0ced3346aafc958461818e
miodas <wielkimiodas@gmail.com>
2013-10-15 23:55:38 +0200

    stemmer pinned, first unit tests added

---
+++ source/SearchEngine.Solver.UnitTest/StemmerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SearchEngine.Solver.Stemmer;
+
+namespace SearchEngine.Solver.UnitTest
+{
+    [TestClass]
+    public class StemmerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var stemmer = new PorterStemmer();
+            var res = stemmer.stemTerm("Available");
+            var res2 = stemmer.stemTermToLowerAndClean("available, But ... yoU know what is available");
+            Debug.WriteLine("Res2: " + res2);
+            var arr = res2.Split(' ');
+        }
+    }
+}
---
+++ source/SearchEngine.Solver/Stemmer/StemmerInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine.Solver.Stemmer
+{
+    interface StemmerInterface
+    {
+        string stemTerm(string s);
+    }
+}
